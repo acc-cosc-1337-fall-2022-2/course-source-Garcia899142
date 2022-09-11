ba716b0bbc2568a7d6170353e0547da682984cbe
Garcia899142 <Jorge.garcia10@g.austincc.edu>
2022-09-11 04:59:38 +0000

    hw-01-data-types

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,6 +1,7 @@
 ﻿//write include statements
-
-
+#include<iostream>
+#include "data_types.h"
+using namespace std;
 //write namespace using statement for cout
 
 
@@ -9,7 +10,16 @@ Call multiply_numbers with 10 and 10 parameter values and display function resul
 */
 int main()
 {
-	
+    int num;
+    cout<<"Enter a number: ";
+    cin>>num;
+    int result;
+    result = multiply_numbers(num);
+    cout<<result<<endl;
 
-	return 0;
+    int num1;
+    num1 = 4;
+    result = multiply_numbers(num1);
+    cout<<result;
+    return 0;
 }
