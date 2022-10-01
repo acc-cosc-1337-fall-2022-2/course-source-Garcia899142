473f2b2c3e78d846204997ba9e43a54d366aa3e0
Garcia899142 <Jorge.garcia10@g.austincc.edu>
2022-10-01 17:49:50 +0000

    hw4

--- src/homework/04_repetition/dna.cpp
+++ src/homework/04_repetition/dna.cpp
@@ -1,3 +1,36 @@
 //add include statements
-
+#include "dna.h"
+#include <iostream>
 //add function(s) code here
+int factorial(int num)
+{
+    int factorial = num;
+    num--;
+    while(num>0)
+    {
+        factorial *= num;
+        num--;
+    
+    }
+    return factorial;
+}
+
+int gcd(int num1, int num2)
+{
+    while (num1 != num2)
+    {
+        if(num1 < num2) 
+        {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+        
+        if (num1 > num2) 
+        {
+            num1 -= num2;
+        }
+    }
+        
+    return num1;
+}
--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,6 +1,8 @@
 //write include statements
-
+#include "dna.h"
+#include <iostream>
 //write using statements
+using namespace std;
 
 
 /*
@@ -12,5 +14,48 @@ user enters a y or Y.
 */
 int main() 
 {
+	char exit = 'N';
+
+	do
+	{	
+		int choice;
+		std::cout<<"1. Factorial \n";
+		std::cout<<"2. Greatest Common Divisor \n";
+		std::cout<<"3. Exit \n";
+		std::cout<<"Choose a function: ";
+		std::cin>>choice;
+		
+		switch(choice) 
+		{
+			case 1:
+			{
+				int num;
+				std::cout<<"Enter a number: ";
+				std::cin>>num;
+				int value = factorial(num);
+				std::cout<<num<<" factorial is "<<value<<"\n";
+			}
+			break;
+			case 2:
+			{
+				int num1, num2;
+				std::cout<<"Enter the first number: ";
+				std::cin>>num1;
+				std::cout<<"Enter the second number: ";
+				std::cin>>num2;
+				int value = gcd(num1, num2);
+				std::cout<<"The Greatest Common Divisor of "<<num1<<" and "<<num2<<" is "<<value<<"\n";
+			}
+			break;
+			case 3:
+			{
+				std::cout<<"Are you sure you want to exit? (Y/N): ";
+				std::cin>>exit;
+			}
+			
+			break;			
+		}
+	}	while (exit != 'Y');
+		
 	return 0;
 }
