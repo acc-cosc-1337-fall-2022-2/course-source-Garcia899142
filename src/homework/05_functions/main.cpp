3838d16e6c0683238895ce31b57a28b2893d3d67
Garcia899142 <Jorge.garcia10@g.austincc.edu>
2022-10-09 00:38:23 +0000

    hw5

--- src/homework/05_functions/main.cpp
+++ src/homework/05_functions/main.cpp
@@ -7,8 +7,34 @@ and display them to screen.
 Program continues until user decides to exit.
 
 */
+#include "func.h"
+#include<iostream>
+
+using std::cout;
+using std::string;
+using std::cin;
+
 int main() 
 {
-
+    int users_choice;
+    do {
+        users_choice = run_menu();
+        if(users_choice == 1) {
+            string dna_str;
+            cout<<"Enter DNA String \n";
+            cin>>dna_str;
+            cout<<get_gc_content(dna_str);
+            cout<<"\n";
+        } else if(users_choice == 2){
+            string dna_str;
+            cout<<"Enter DNA String \n";
+            cin>>dna_str;
+            cout<<get_dna_complement(dna_str);
+            cout<<"\n";
+        }
+        
+    }
+    while(users_choice != 3);
+    
 	return 0;
 }
