122e3e10c0a3c5db903da7663d6cdaeadeac0cfe
Garcia899142 <Jorge.garcia10@g.austincc.edu>
2022-10-29 21:33:02 +0000

    hw6

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,6 +1,47 @@
+#include "tic_tac_toe.h"
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::cout;
+using std::cin;
 
 int main() 
-{
+{	
+	tic_tac_toe game;
+	string starting_player;
+	int position;
+	bool loop_again;
+
+	do {
+
+		cout << "Enter X or O: ";
+		cin >> starting_player;
+
+		while (starting_player != "X" && starting_player != "O") {
+			cout << "Invalid input try again" << "\n";
+			cout << "Enter X or O: ";
+			cin >> starting_player;
+		}
+
+		game.start_game(starting_player);
+
+		do {
+			
+			game.display_board();
+			cout << "Enter a number from 1 to 9: ";
+			cin >> position;
+
+			game.mark_board(position);
+
+
+		} while(game.game_over() == false);
+		game.display_board();
+
+		cout << "\n" << "Do you want to play again (1 for yes 0 for no): ";
+		cin >> loop_again;
+
+	} while (loop_again);
 
 	return 0;
 }
--- src/homework/06_tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/06_tic_tac_toe/tic_tac_toe.cpp
@@ -1,1 +1,68 @@
 //cpp
+#include "tic_tac_toe.h"
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+using std::string;
+using std::vector;
+using std::cout;
+using std::cin;
+
+void tic_tac_toe::clear_board() {
+    pegs = {" ", " ", " ",
+            " ", " ", " ",
+            " ", " ", " ",};
+}
+
+void tic_tac_toe::display_board() const {
+    string output = "";
+
+        output =("\033[4m" + pegs.at(0) + "|" + pegs.at(1) + "|" + pegs.at(2) + "\n" +
+                             pegs.at(3) + "|" + pegs.at(4) + "|" + pegs.at(5) + "\033[0m" + "\n" + 
+                             pegs.at(6) + "|" + pegs.at(7) + "|" + pegs.at(8) + "\n");
+
+    cout << output;
+
+}
+
+void tic_tac_toe::mark_board(int position) {
+    pegs.at(position-1) = player;
+    set_next_player();
+}
+
+void tic_tac_toe::set_next_player() {
+    if (player == "X") {
+        player = "O";
+    } else {
+        player = "X";
+    }
+}
+
+bool tic_tac_toe::check_board_full() {
+    int couter = 0;
+    bool output = 1;
+
+    while (couter < 9) {
+        if (pegs.at(couter) == " ") {
+            output = 0;
+        }
+        couter++;
+    }
+    return output;
+}
+
+string tic_tac_toe::get_player() const {
+    return player;
+}
+
+void tic_tac_toe::start_game(string first_player) {
+    player = first_player;
+    clear_board();
+}
+
+bool tic_tac_toe::game_over() {
+
+    return check_board_full();
+}
