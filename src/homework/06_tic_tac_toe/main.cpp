17854083a311f1d8af452c5252233fca54024637
Garcia899142 <Jorge.garcia10@g.austincc.edu>
2022-11-05 18:33:14 -0500

    hw6

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,27 +1,26 @@
 #include "tic_tac_toe.h"
-
-
 int main() 
 {
-	TicTacToe game;
-	string first_player;
-	char end = 'N';
-	int position;
-	cout << "Tic-Tac-Toe Time!\n";
-	cout << "Moves you can make:\n1|2|3\n4|5|6\n7|8|9\n";
-	do {
-		cout<<"Player 1 choose X or O: ";
-		cin>>first_player;
-		game.start_game(first_player);
-		while (!game.game_over()) {
-			game.display_board();
-			cout << "Player "<<game.get_player()<<", select position 1-9: ";
-			cin >> position;
-			game.mark_board(position);
-		}
-		game.display_board();
-		cout<<"\nExit program? (Y/N): ";
-		cin>>end;
-	} while(end != 'Y');
+    TicTacToe game;
+    string first_player, play_again, winner;
+    int position;
+    do {
+        cout << "Do you wish to play 'X' or 'O'?\n";
+        cin >> first_player;
+        game.start_game(first_player);
+        while (!game.game_over()) {
+            game.display_board();
+            cin >> position;
+            game.mark_board(position - 1);
+        }
+        winner = game.get_winner();
+        cout << "Game over!";
+        if (winner == "C") cout << " It's... a tie? Boring!";
+        else cout << " And the winner is " << winner << "!\n";
+        cout << "\n\nPlay again? (Y/N)\n";
+        cin >> play_again;
+    }
+    while (toupper(play_again[0]) == 'Y');
+    cout << "Thanks for playing!";
 	return 0;
 }
--- src/homework/06_tic_tac_toe/tic_tac_toe.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe.h
@@ -1,24 +1,30 @@
+//h
 #include <iostream>
-#include <algorithm>
-#include <vector>
 #include <string>
-using std::vector;
-using std::string;
-using std::cin;
-using std::cout;
+#include <vector>
+using namespace std;
 
-class TicTacToe{
+class TicTacToe {
+    vector<string> pegs{" "," "," "," "," "," "," "," "," "};
+    string player;
+    string winner;
+
 public:
+    string choices = "123456789";
     bool game_over();
     void start_game(string first_player);
     void mark_board(int position);
+    bool check_section(int position);
     string get_player() const;
     void display_board() const;
+    string get_winner();
 
 private:
     void set_next_player();
     bool check_board_full();
     void clear_board();
-    string player;
-    vector<string> pegs{" "," "," "," "," "," "," "," "," "};
+    bool check_column_win();
+    bool check_row_win();
+    bool check_diagonal_win();
+    void set_winner();
 };
